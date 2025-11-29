a138693600506f168c9ad8da2c4f6705b3c2224a
1568018+davassi@users.noreply.github.com <gianluigi.davassi@gmail.com>
2025-11-29 17:18:44 +0100

    bash & fish scripts

--- hypertrade-prod.sh
+++ hypertrade-prod.sh
@@ -1,11 +1,11 @@
-#!/usr/bin/env fish
+#!/usr/bin/env bash
 
 # Export env vars from pass
-set -x HYPERTRADE_MASTER_ADDR     (pass show hypertrade/master_addr | head -n 1)
-set -x HYPERTRADE_API_WALLET_PRIV (pass show hypertrade/api_wallet_priv | head -n 1)
-set -x HYPERTRADE_SUBACCOUNT_ADDR (pass show hypertrade/subaccount_addr | head -n 1)
-set -x HYPERTRADE_LISTEN_PORT     6487
-set -x HYPERTRADE_DB_PATH         ./hypertrade_prod.db
+export HYPERTRADE_MASTER_ADDR=$(pass show hypertrade/master_addr | head -n 1)
+export HYPERTRADE_API_WALLET_PRIV=$(pass show hypertrade/api_wallet_priv | head -n 1)
+export HYPERTRADE_SUBACCOUNT_ADDR=$(pass show hypertrade/subaccount_addr | head -n 1)
+export HYPERTRADE_LISTEN_PORT=6487
+export HYPERTRADE_DB_PATH=./hypertrade_prod.db
 
 # Launch the server
-uvicorn hypertrade.daemon:app --host 0.0.0.0 --port $HYPERTRADE_LISTEN_PORT --workers 1 --loop uvloop --http httptools --log-level info --access-log --use-colors --limit-concurrency 1000 
+uvicorn hypertrade.daemon:app --host 0.0.0.0 --port $HYPERTRADE_LISTEN_PORT --workers 1 --loop uvloop --http httptools --log-level info --access-log --use-colors --limit-concurrency 1000
